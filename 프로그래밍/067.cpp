e3f60222434295c1f2f6a84ee7c73270c8dd92bc
126-134340 <86914459+126-134340@users.noreply.github.com>
2021-10-04 21:55:03 +0900

    프로그래밍 p.67 <변수 선언 응용하기>

--- 프로그래밍/067.cpp
+++ 프로그래밍/067.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	char myBloodType = 'A';	//char형 변수를 선언하고 문자'A'를 할당 
 	int age, schoolNumber; 
-	double weight;	//double형 변수 weight를 선언 
+	double weight;	//double형 변수 weight를 선언
 	age = 18; schoolNumber = 12;
 	weight = 173.4;	//weight에 실수값 173.4를 할당 
 	return 0;
