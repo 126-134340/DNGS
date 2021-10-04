8ef87678297b3af39bd5b9f0649a5fa6025e366d
126-134340 <86914459+126-134340@users.noreply.github.com>
2021-10-04 21:53:59 +0900

    Rename 77.cpp to 077.cpp

---
+++ 프로그래밍/077.cpp
@@ -0,0 +1,24 @@
+//프로그래밍 p.77 <제어 문자로 숫자 변환하기>
+ 
+#include <stdio.h>
+int main()
+{
+	int i = 680;
+	printf("10진수=");
+	printf("%d\n",i);
+	printf("8진수=");
+	printf("%o\n",i);
+	printf("16진수=");
+	printf("%x\n",i);
+	return 0;
+}
+
+
+//[INPUT]
+//(없음)
+//
+//
+//[OUTPUT]
+//10진수=680
+//8진수=1250
+//16진수=2a8
