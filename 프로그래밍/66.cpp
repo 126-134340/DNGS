0b8112af17e56ac1c465b00df0f5fcee5a5dc0df
126-134340 <86914459+126-134340@users.noreply.github.com>
2021-10-04 21:34:39 +0900

    프로그래밍 p.66 <변수 선언>

---
+++ 프로그래밍/66.cpp
@@ -0,0 +1,18 @@
+//프로그래밍 p.66 <변수 선언> 
+
+#include <stdio.h>
+int main()
+{
+	char myChar;	//문자형 변수 myChar 선언 
+	int count, resultNumber;	//정수형 변수 count, resultNumber 선언 
+	double weight;	//실수형 변수 weight 선언 
+	return 0;
+}
+
+
+//[INPUT]
+//(없음)
+//
+//
+//[OUTPUT]
+//(없음)
