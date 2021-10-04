651721318940f298a7a87747230e59fbe983bb56
126-134340 <86914459+126-134340@users.noreply.github.com>
2021-10-04 23:00:15 +0900

    블루투스 모듈

---
+++ ARDUINO/TEST/bluetooth.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(4, 5);
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Hello!");
+  BTSerial.begin(9600);
+}
+
+void loop(){
+  while(BTSerial.available()){
+    byte data = BTSerial.read();
+    Serial.write(data);
+  }
+
+  while(Serial.available()){
+    byte data = Serial.read();
+    BTSerial.write(data);
+  }
+}
